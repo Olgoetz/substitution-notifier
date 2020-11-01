98a428635d5356c0f5d1bb34c6b785ffb0a0b834
Oliver Götz <goetzoliver89@gmail.com>
2020-11-01 19:44:13 +0100

    removed config.py

--- src/lib/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ[
-    "SUBSTITUTION_NOTIFIER_RECIPIENT"] = "Juliane.Pollheide@club.fitnessfirst.de"
-os.environ["SUBSTITUTION_NOTIFIER_LOG"] = "INFO"
-os.environ[
-    "SUBSTITUTION_NOTIFIER_LOGFORMAT"] = "%(asctime)-15s %(levelname)-8s %(name)-18s %(message)s"
